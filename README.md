ba4c368b3b6d0c7bbe6cf680912d89809fefce82
Sebastian Brown <developer2025@gmail.com>
2019-02-18 23:05:56 +1100

    Elvanto setup

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 This form is designed to take parent input and forward it to Elvanto as a Wave registration.
 
 # Setup
-Clone this repo and then add a file to the root directory named `api-key.php`
-This file is used to define the API key. Use it to define the variable `$api_key`.
+Clone this repo and then add a file to the root directory named `api-keys.php`
+This file is used to define the API tokens. Use it to define the variable `$api_key`.
 E.g.
 ```
 <?php
--- process.php
+++ process.php
@@ -7,8 +7,8 @@
 
 require_once 'debug.php';
 require_once 'Elvanto_API.php';
-if(!@include_once('api-key.php')) {
-  die('NO API KEY - make a file named api-key.php that defines the variable $api_key');
+if(!@include_once('api-keys.php')) {
+  die('NO API KEY - make a file named api-keys.php that defines the variable $api_key');
 }
 
 date_default_timezone_set ('Australia/Sydney');
@@ -33,6 +33,31 @@ fclose($myfile);
 
 echo "Sending to Elvanto...\n";
 
-echo "Will now redirect to the payment jotform...\n";
+/*
+Here's the deal:
+- Instantiate the API Wrapper.
+- Search for the primary contact to see if they are known to HBC.
+- If not, create a person for primary, with a new family.
+- If they are, get the person's family Id.
+- Add the parent to the wave parent group.
+- Search each child with the family id as a parameter. We don't care about any matches outside the family.
+- If any children are found, add them to the wave attendees group.
+- If not found, create a person, add to family and add to attendee group.
+*/
+
+
+$elvanto = new Elvanto_API(array('api_key' => $api_key));
+$request = [
+  "search" => [
+    "firstname" => "Sebastian",
+    "lastname" => "Brown"
+  ]
+];
+$results = $elvanto->call('people/search',$request);
+debug($results);
+
+
+//echo "Will now redirect to the payment jotform...\n";
+
 
  ?>
